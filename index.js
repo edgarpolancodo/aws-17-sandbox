673208ae315ee3ca081582f61693257b4456ce9f
Edgar Polanco <theboss.edgar14@gmail.com>
2017-03-01 18:16:22 +0000

    primera version

---
+++ index.js
@@ -0,0 +1,42 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var bodyParser = require('body-parser');
+var port = (process.env.PORT || 3000);
+var app = express();
+
+
+var contacts = [{
+    name: 'Edgar',
+    phone: '809829849',
+    email: 'edgar@edgar.com'
+}, {
+    name: 'Rosangel',
+    phone: '849829809',
+    email: 'rosangel@rosangel.com'
+}];
+
+app.use(bodyParser.json());
+
+var base ='/api/v1'
+
+//console.log(cool());
+app.get(base+'/contacts', (req, res) => {
+    res.send(contacts);
+    console.log('New request');
+});
+
+app.post(base+'/contacts', (req, res) => {
+    var contact = req.body;
+    contacts.push(contact);
+    res.sendStatus(201);
+    console.log('New request');
+});
+
+app.get('/', (req, res) => {
+    res.send('<html><body><h1>Hola cliente' + cool() + '</h1></body></html>');
+    console.log('New request');
+});
+
+app.listen(port, () => {
+    console.log('servidor corriendo...' + process.env.IP)
+});
