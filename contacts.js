10c92b4a8ce1861511f8c748fa03834b65259652
Edgar Polanco <theboss.edgar14@gmail.com>
2017-03-15 18:02:54 +0000

    v2 Grunt tests

--- contacts.js
+++ contacts.js
@@ -1,8 +1,8 @@
 'use strict';
-var cool = require('cool-ascii-faces');
+//var cool = require('cool-ascii-faces');
 var express = require('express');
-var bodyParser = require('body-parser');
-var port = (process.env.PORT || 3000);
+//var bodyParser = require('body-parser');
+//var port = (process.env.PORT || 3000);
 var app = express();
 var path = require('path');
 var DataStore = require('nedb');
